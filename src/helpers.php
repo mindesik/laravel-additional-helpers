18ac723da7f8459dadb1d5960b50fa18b18f1f45
De Sik Min <e.min@milax.com>
2017-02-13 18:16:26 +1100

    Fix path_unique function

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,6 @@ use MisterPaladin\LaravelAdditionalHelpers\Helper;
 if (!function_exists('path_unique')) {
     
     function path_unique($path, $suffixLength = 3) {
-        return HelperClass::path_unique($path, $suffixLength);
+        return Helper::path_unique($path, $suffixLength);
     }
 }
